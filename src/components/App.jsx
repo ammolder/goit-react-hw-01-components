cb070502406f8fe9dad641c25e91dae606e891dc
ammolder <98980721+ammolder@users.noreply.github.com>
2022-10-12 12:59:50 +0200

    change way file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import user from './json/user.json';
-import data from './json/data.json';
-import friends from './json/friends.json';
-import transactions from './json/transactions.json';
+import user from '../json/user.json';
+import data from '../json/data.json';
+import friends from '../json/friends.json';
+import transactions from '../json/transactions.json';
 import { Profile } from './ProfileBox/Profile/Profile';
 import { Statistics } from './StatisticsBox/Statistics/Statistics';
 import { FriendList } from './Friend/FrienList/FriendList';
